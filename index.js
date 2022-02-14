184443b4dd323caa1f2b24a201588b9670e992c4
austnaa <60988742+austnaa@users.noreply.github.com>
2022-02-14 10:51:50 -0800

    added post req fetch for posting a task

--- index.js
+++ index.js
@@ -18,4 +18,62 @@ function getTestSql() {
     });
 }
 
-getTestSql();
+// EXAMPLE OF GET FUNCTION WITH PARAMS
+// gets all tasks for a partuclar user
+function getAllTasksForAParticularUser(userid) {
+    fetch('https://tcss445-plan-it.herokuapp.com/gettasks/' + userid, {
+        mode: 'cors',
+        method: 'GET',
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('b1').innerHTML = JSON.stringify(data);
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+
+// EXAMPLE OF POST FUNCTION WITH PARAMS
+// requires param obj like: 
+/* 
+{
+    task_id: 1,
+    task_name: "Quiz 1",
+    description: "Quiz 1 (individual)",
+    priority: 1,
+    category: 1,
+    complete: false,
+    user_id: 1,
+    team_id: null
+}
+*/
+function postTask(taskData) {
+    fetch('https://tcss445-plan-it.herokuapp.com/posttasks/', {
+        mode: 'cors',
+        method: 'POST',
+        body: JSON.stringify(taskData),
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('b1').innerHTML = JSON.stringify(data);
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+getAllTasksForAParticularUser(1);
+
+// postTask({
+//     task_name: "Quiz ",
+//     description: "Quiz 3 (individual)",
+//     priority: 1,
+//     category: 1,
+//     complete: false,
+//     user_id: 1,
+//     team_id: null
+// });
