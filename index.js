6bf4fab38e3a83d01a8d5ac02887dcfe54316397
austnaa <60988742+austnaa@users.noreply.github.com>
2022-02-19 10:19:07 -0800

    added register, signing, and bearer token to get and post tasks

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ function getTestSql() {
     });
 }
 
+
+
 // EXAMPLE OF GET FUNCTION WITH PARAMS
 // gets all tasks for a partuclar user
-function getAllTasksForAParticularUser(userid) {
+function getAllTasksForAParticularUser(userid, token) {
+    let headers = new Headers();
+    headers.set('authorization', "Bearer " + token);
     fetch('https://tcss445-plan-it.herokuapp.com/gettasks/' + userid, {
         mode: 'cors',
         method: 'GET',
@@ -66,14 +70,80 @@ function postTask(taskData) {
     });
 }
 
-getAllTasksForAParticularUser(1);
+/* 
+ * Attempts to register a new user based on the given username,
+ * email, and password.
+ * 
+ * Expects a body like the following:
+ *  {
+ *      username: "theUsername",
+ *      email: "theEmail",
+ *      password: "thePassword"
+ *  }
+ */
+function register(body) {
+    fetch('https://tcss445-plan-it.herokuapp.com/register', {
+        mode: 'cors',
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('b1').innerHTML = JSON.stringify(data);
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+/* 
+ * Attempts to sign in the user based on the given email and password
+ */
+function signIn(email, password) {
+    let headers = new Headers();
+    headers.set('Authorization', 'Basic ' + btoa(email + ":" + password));
+    
+    fetch('https://tcss445-plan-it.herokuapp.com/signin', {
+        mode: 'cors',
+        method: 'GET',
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('b1').innerHTML = JSON.stringify(data);
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+
+
+
+// signIn("email1@email.com", "thePassword");
+
+
+
+
+
+
+
+getAllTasksForAParticularUser(1, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsMUBlbWFpbC5jb20iLCJ1c2VyaWQiOjE5LCJ1c2VybmFtZSI6InRoZVVuYW1lMSIsImlhdCI6MTY0NTI5Mjc2NCwiZXhwIjoxNjQ2NTAyMzY0fQ.xpWp9NZ9X6aYAZ9RNfR9z-mZqwGfY5Tx-vCK5Pq4DP0");
+
+
+// register({ 
+//     username: "theUname1",
+//     email: "email1@email.com",
+//     password: "thePassword"
+// });
 
 // postTask({
-//     task_name: "Quiz ",
-//     description: "Quiz 3 (individual)",
-//     priority: 1,
+//     task_name: "Quiz 10",
+//     description: "Quiz 10 (individual)",
+//     priority: 2,
 //     category: 1,
-//     complete: false,
+//     complete: true,
 //     user_id: 1,
 //     team_id: null
 // });
